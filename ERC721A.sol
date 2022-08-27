c8db57194c744f9dc6c0267061aaa80926265632
Chris Ekom <ekomchris2016@gmail.com>
2022-08-27 01:05:44 +0100

    Create ERC721A.sol

---
+++ ERC721A.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.4
+
+import "erc721a/contracts/ERC721A.sol";
+
+contract Azuki is ERC721A {
+  constructor() ERC721A("Azuki", "AZUKI") {}
+  function mint(uint256 quantity) external payable {
+    // _safeMint's second argument now takes in a quantity, not a tokenId.
+    _safeMint(msg.sender, quantity);
+  }
+}
